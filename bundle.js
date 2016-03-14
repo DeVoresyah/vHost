fca284e30b4f266460fba02e47a498be9e89a72f
DeVoresyah <de.voresyah@gmail.com>
2016-03-14 11:18:57 +0700

    [Add] bundle.js

---
+++ bundle.js
@@ -0,0 +1,3 @@
+	"console" in window && console.log("%cHi developer\nif you find anything unusual please report on the our website %chttp://www.vorecorp.id",
+		"color: #2c3e50; font-size: 17px; line-height: 2",
+		"color: #c0392b; font-size: 17px; line-height: 2; font-style: normal");
